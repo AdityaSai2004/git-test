677ee2a174ba528b56f289645ff221e4f6c9b180
aruneer007 <arunvenkatachalam895970.gmail.com>
2023-11-17 12:32:51 +0530

    decode

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 # filename: ssh_script.py
 import paramiko
 import os
+from base64 import b64decode
 
 # Replace these variables with your actual values
 hostname = '13.126.13.44'
 port = 22
 username = 'ec2-user'
-private_key_path = 'private_key2.pem'
+private_key_path = 'private_key.pem'
+
+private_key_content = b64decode(private_key_path).decode()
+
+print(private_key_content)
 
 # Create an SSH client
 client = paramiko.SSHClient()
@@ -18,15 +23,39 @@ private_key = paramiko.RSAKey(filename=private_key_path)
 # Connect to the server
 client.connect(hostname, port=port, username=username, pkey=private_key)
 
-# Run a command on the server
-stdin, stdout, stderr = client.exec_command('ls')
+# # Run a command on the server
+# stdin, stdout, stderr = client.exec_command('ls')
 
-# Print the output
-print("Output:")
-print(stdout.read().decode())
+# # Print the output
+# print("Output:")
+# print(stdout.read().decode())
 
-# Close the connection
-client.close()
+# # Close the connection
+# client.close()
+
+try:
+    # Run multiple commands on the server
+    commands = [
+        'git clone https://github.com/aruneer007/git-test.git ',
+        'cd git-test',
+        'python hello.py',
+        # Add more commands as needed
+    ]
+
+    for command in commands:
+        print(f"Executing command: {command}")
+        stdin, stdout, stderr = client.exec_command(command)
+        
+        # Print the command output
+        print(stdout.read().decode())
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    # Close the connection
+    client.close()
+
 
 # with open(private_key_path, 'r') as file:
 #     pem_contents = file.read()
